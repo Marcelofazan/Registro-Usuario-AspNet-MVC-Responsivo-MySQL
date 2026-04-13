0740afff2fe4e08aa8411f8a0fc0ef2b7912cca3
Marcelo Gustavo Fazan <56393266+Marcelofazan@users.noreply.github.com>
2026-04-13 09:11:02 -0300

    Simplify Usuarios table structure

--- ERPSimplesLTE/Database/Usuarios.sql
+++ ERPSimplesLTE/Database/Usuarios.sql
@@ -1,12 +1,8 @@
 CREATE TABLE Usuarios (
-  Id int(11) NOT NULL AUTO_INCREMENT,
-  Login varchar(100) DEFAULT NULL,
-  Senha varchar(20) DEFAULT NULL,
-  SenhaConfirmar varchar(20) DEFAULT NULL,
+  Id int NOT NULL AUTO_INCREMENT,
   Nome varchar(80) DEFAULT NULL,
   Email varchar(120) DEFAULT NULL,
-  Celular varchar(15) DEFAULT NULL,
-  Supervisor int(11) DEFAULT 0,
-  TaxaPercentual double(5,2) DEFAULT NULL,
+  Senha varchar(20) DEFAULT NULL,
+  SenhaConfirmar varchar(20) DEFAULT NULL,
   PRIMARY KEY (Id)
 );
